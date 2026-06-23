136ca973b39b460776f691c748a03296f571fe5c
JateenDhaduk <jateendhaduk456@gmail.com>
2026-06-23 22:28:40 +0530

    Time: 22 ms (41.9%), Space: 50.4 MB (90.45%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+       Map<Integer, Integer> prefixCount = new HashMap<>();
+
+       int sum = 0;
+       int count = 0;
+       prefixCount.put(0,1);
+
+       for(int num : nums){
+            sum += num;
+            count += prefixCount.getOrDefault(sum - goal, 0);
+            prefixCount.put(sum , prefixCount.getOrDefault(sum,0) + 1);
+       }
+       return count;
+    }
+
+}
